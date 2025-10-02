7bbdfadbac6734b98c50b157c83508ab35a27d4a
damianfral <huevofritopamojarpan@gmail.com>
2025-10-02 01:20:00 +0000

    Add GenValidity instances for EmailAddress.

--- src/Data/EmailAddress.hs
+++ src/Data/EmailAddress.hs
@@ -5,6 +5,8 @@
 module Data.EmailAddress where
 
 import Data.Aeson
+import Data.GenValidity
+import Data.GenValidity.Text ()
 import qualified Database.PostgreSQL.Simple.FromField as PG
 import qualified Database.PostgreSQL.Simple.ToField as PG
 import Relude
@@ -13,3 +15,4 @@ import Servant (ToHttpApiData)
 newtype EmailAddress = EmailAddress {unEmailAddress :: Text}
   deriving newtype (Show, PG.ToField, PG.FromField, ToJSON, FromJSON, ToHttpApiData, Eq, IsString)
   deriving (Generic)
+  deriving newtype (Validity, GenValid)
